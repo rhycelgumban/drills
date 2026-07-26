66af196099bf073ee484725eafe4943b8b25cb36
Rhycel Gumban <rhycelgumban@github.com>
2026-07-26 20:43:09 +0800

    feat: add reallife jscript drill

---
+++ drill-5-2026-07-21-reallife-jscript/kape.js
@@ -0,0 +1,12 @@
+function kape() {
+    let mayKwarta = true;
+    let bukasAngCafe = false;
+
+    if (mayKwarta && bukasAngCafe) {
+        console.log("Mabakál ko kape.");
+    } else {
+        console.log("Sa balay na lang ko magkape.");
+    }
+}
+
+kape();
